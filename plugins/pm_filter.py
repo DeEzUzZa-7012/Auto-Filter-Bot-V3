6572408d01016e1d72f29f3b1183f146eb767811
DeEzUzZa-7012 <108201261+DeEzUzZa-7012@users.noreply.github.com>
2022-06-29 10:09:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -87,6 +87,7 @@ async def filter(client, message):
             buttons = btn
             buttons.append(
                 [InlineKeyboardButton(text="📜 Pages 1/1",callback_data="pages")]
+             
             )
             poster=None
             if API_KEY:
@@ -103,8 +104,8 @@ async def filter(client, message):
 
         buttons.append(
             [InlineKeyboardButton(text="NEXT »»",callback_data=f"next_0_{keyword}")
-             InlineKeyboardButton(text="📜 Pages 1/1",callback_data="pages")]
-        )    
+             InlineKeyboardButton(text="📜 Pages 1/1",callback_data="pages")])
+           
         buttons.append(
             [InlineKeyboardButton(text=f"📃 Pages 1/{data['total']}",callback_data="pages")]
         )
