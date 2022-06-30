cb6b3475ba652026a6093af033902eae69b7e951
DeEzUzZa-7012 <108201261+DeEzUzZa-7012@users.noreply.github.com>
2022-06-30 10:32:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -169,7 +169,8 @@ async def group(client, message):
         buttons = data['buttons'][0].copy()
 
         buttons.append(
-            [InlineKeyboardButton(text="NEXT »»",callback_data=f"next_0_{keyword}")]
+            [InlineKeyboardButton(text="NEXT »»",callback_data=f"next_0_{keyword}"),
+             InlineKeyboardButton(text=f"📜 Pages 1/{data['total']}",callback_data="pages")]
         )    
         buttons.append(
             [InlineKeyboardButton(text=f"📜 Pages 1/{data['total']}",callback_data="pages")]
